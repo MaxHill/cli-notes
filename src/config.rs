291eb4cfbb00f84681e44a01c81d9beb3eec8559
Max Hill <max@hilloco.se>
2023-11-29 10:00:07 +0100

    refactor: refactor new note and add meta to root command

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,13 @@
 use anyhow::Context;
+use clap::ArgMatches;
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
-    fs,
+    fs::{self},
     path::{Path, PathBuf},
 };
+
+use crate::utils::parse_metadata;
 
 fn default_file_md() -> String {
     "md".to_string()
@@ -22,17 +25,21 @@ pub struct Config {
 }
 
 impl Config {
-    // pub fn new_from_path(config_dir_path: PathBuf) -> anyhow::Result<Config>
-    pub fn new_from_path<P>(config_path: P) -> anyhow::Result<Config>
-    where
-        std::path::PathBuf: std::convert::From<P>,
-    {
-        let filename = PathBuf::from(config_path).join("config.toml");
+    pub fn try_new(matches: &ArgMatches) -> anyhow::Result<Config> {
+        let config_path = get_config_path(matches.get_one::<String>("config-path"))?;
+        print!("CONFIG: {:?}", config_path);
+        let additional_metadata = parse_metadata(matches.get_many::<String>("meta-data"));
+
+        // let filename = PathBuf::from(config_path).join("config.toml");
+        let filename = config_path.join("config.toml");
+        print!("FILENAME:{:?}", filename);
 
         let contents = fs::read_to_string(&filename)
             .with_context(|| format!("Could not find config file: {:?}", filename))?;
+
         toml::from_str::<Config>(&contents)
             .with_context(|| format!("Could not construct Config from {:?}", contents))
+            .map(|c| c.clone_add_meta(additional_metadata))
     }
 
     pub fn clone_add_meta(&self, meta: HashMap<String, String>) -> Config {
@@ -41,6 +48,7 @@ impl Config {
         c.clone()
     }
 }
+
 pub fn get_config_path(flag: Option<&String>) -> anyhow::Result<PathBuf> {
     let path_flag = flag.map(|c| Path::new(c).to_path_buf()).ok_or(());
 
@@ -58,10 +66,41 @@ pub fn get_config_path(flag: Option<&String>) -> anyhow::Result<PathBuf> {
 mod test {
 
     use super::*;
+    use clap::{Arg, ArgAction, Command};
+    use predicates::{prelude::predicate, Predicate};
+
+    fn get_matches_from<S>(dir: S, flags: Option<Vec<&str>>) -> clap::ArgMatches
+    where
+        S: std::convert::AsRef<std::path::Path>,
+    {
+        let binding = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(dir);
+        let d = binding.to_str().unwrap();
+
+        let f = [vec!["--config-path", d], flags.unwrap_or(vec![])].concat();
+        println!("F: {:?}", f);
+
+        Command::new("test")
+            .no_binary_name(true)
+            .arg(
+                Arg::new("config-path")
+                    .short('c')
+                    .long("config-path")
+                    .help("Provide a path to the config directory. Default is $XDG_CONFIG_HOME"),
+            )
+            .arg(
+                Arg::new("meta-data")
+                    .short('m')
+                    .long("meta-data")
+                    .value_name("KEY:VALUE")
+                    .action(ArgAction::Append)
+                    .help("Aditional key value pairs to be added to config. Ex. --meta-data name:John"),
+            )
+            .get_matches_from(f)
+    }
 
     #[test]
     fn it_can_get_config_from_file() {
-        match Config::new_from_path("./test-config/") {
+        match Config::try_new(&get_matches_from("test-config", None)) {
             Ok(config) => {
                 assert_eq!(config.notes_dir, PathBuf::from("/tmp/cli-notes-test-dir"));
             }
@@ -73,15 +112,30 @@ mod test {
 
     #[test]
     fn it_fails_if_config_does_not_exist() {
-        match Config::new_from_path("./does-not-exist/") {
+        match Config::try_new(&get_matches_from("does-not-exist", None)) {
             Ok(config) => {
                 panic!("Did not error when it should have: {:?}", config);
             }
             Err(e) => {
-                assert_eq!(
-                    format!("{}", e),
-                    "Could not find config file: \"./does-not-exist/config.toml\""
+                assert!(
+                    predicate::str::contains("Could not find config file:").eval(&e.to_string())
                 );
+                assert!(predicate::str::contains("does-not-exist/config.toml").eval(&e.to_string()));
+            }
+        };
+    }
+
+    #[test]
+    fn adds_metadata_to_configs_metadata() {
+        match Config::try_new(&get_matches_from(
+            "test-config",
+            Some(vec!["--meta-data", "some:value"]),
+        )) {
+            Ok(config) => {
+                assert_eq!("value", config.meta.get("some").unwrap());
+            }
+            Err(e) => {
+                panic!("Error when getting config: {}", e);
             }
         };
     }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use crate::{config::get_config_path, note_from_template::NoteFromTemplate};
-use anyhow::anyhow;
-use clap::{command, parser::ValuesRef, Arg, ArgAction, Command};
+use crate::new_note::NewNote;
+use clap::{command, Arg, ArgAction, Command};
 use config::Config;
-use std::{collections::HashMap, ffi::OsString, os::unix::process::CommandExt};
 use subcommand::SubCommand;
 
 pub mod config;
-pub mod note_from_template;
+pub mod new_note;
 pub mod subcommand;
 pub mod templating;
+pub mod utils;
 
-fn cmd() -> Command {
+pub fn cmd() -> Command {
     command!() // requires `cargo` feature
         .propagate_version(true)
         .subcommand_required(true)
@@ -22,71 +21,27 @@ fn cmd() -> Command {
                 .long("config-path")
                 .help("Provide a path to the config directory. Default is $XDG_CONFIG_HOME"),
         )
-        .subcommand(new())
-}
-
-fn new() -> Command {
-    Command::new("new")
-        .about("Creates note from specified template")
-        .arg(
-            Arg::new("name")
-                .required(true)
-                .help("Name of the note"),
-        )
-        .arg(
-            Arg::new("template")
-                .short('t')
-                .long("template")
-                .value_name("TEMPLATE_NAME")
-                .help("Handlebars template file to be used. Ex. given template file: ./template/my-template.hbs Flag should look like this: --template my-template "),
-        )
-        .arg(
-            Arg::new("name-template")
-                .short('n')
-                .long("name-template")
-                .value_name("TEMPLATE_STRING")
-                .help("Handlebars template string for name. Ex. --name_template {{date now}}_{{name}}"),
-        )
         .arg(
             Arg::new("meta-data")
                 .short('m')
                 .long("meta-data")
                 .value_name("KEY:VALUE")
                 .action(ArgAction::Append)
-                .help("Key value to be passed to template. Ex. --meta-data name:John"),
+                .help("Aditional key value pairs to be added to config. Ex. --meta-data name:John"),
         )
-}
-
-fn parse_metadata(meta: Option<ValuesRef<'_, String>>) -> HashMap<String, String> {
-    meta.unwrap_or_default()
-        .filter_map(|v| {
-            v.split_once(':')
-                .map(|(key, value)| (key.to_string(), value.to_string()))
-        })
-        .collect()
+        .subcommand(NewNote::cmd())
 }
 
 fn main() -> anyhow::Result<()> {
     let matches = cmd().get_matches();
 
-    let config_path = get_config_path(matches.get_one::<String>("config-path"))?;
-    let config = Config::new_from_path(config_path)?;
+    let config = Config::try_new(&matches)?;
 
     match matches.subcommand() {
-        Some(("new", sub_matches)) => {
-            let meta = parse_metadata(sub_matches.get_many::<String>("meta-data"));
-            let template = sub_matches.get_one::<String>("template");
-            let name = sub_matches
-                .get_one::<String>("name")
-                .ok_or(anyhow!("No name specified"))?;
-            let name_template = sub_matches.get_one::<String>("name-template");
-            // let created_file = create_from_template(&config, "test-template", "test-template.md")?;
-            let note_from_template =
-                NoteFromTemplate::new(&config, template, name.to_string(), name_template, meta);
-            let created_file = note_from_template.write()?;
-
-            println!("{}", created_file.display());
-        }
+        Some(("new", sub_matches)) => println!(
+            "{}",
+            NewNote::try_new(&config, sub_matches)?.write()?.display()
+        ),
         Some(matching) => SubCommand::try_new(&config, matching)?.run()?,
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
